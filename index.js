c91ed239a9d52b37b8678ed889aa71a38adec14a
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2018-11-27 18:48:08 +1000

    fix: upgrade ut-port

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 module.exports = function(modules, destination = 'db') {
     const dispatchToDB = new RegExp('^(' + modules.join('|') + ')\\.');
-    return {
-        id: modules.join('-'),
-        createPort: require('ut-port-script'),
-        logLevel: 'trace',
-        imports: modules,
-        concurrency: 200,
-        exec: function(msg, $meta) {
-            if ($meta.method && dispatchToDB.test($meta.method)) {
-                return this.bus.importMethod(destination + '/' + $meta.method)(msg, $meta);
-            } else {
-                return Promise.reject(this.bus.errors.methodNotFound({params: {method: $meta && $meta.method}}));
+    return (...params) => ({
+        [modules[0] + 'Dispatch']: class extends require('ut-port-script')(...params) {
+            get defaults() {
+                return {
+                    imports: modules,
+                    concurrency: 200
+                };
+            }
+            handlers() {
+                return {
+                    exec: (msg, $meta) => {
+                        if ($meta.method && dispatchToDB.test($meta.method)) {
+                            return this.bus.importMethod(destination + '/' + $meta.method)(msg, $meta);
+                        } else {
+                            return Promise.reject(this.bus.errors.methodNotFound({params: {method: $meta && $meta.method}}));
+                        }
+                    }
+                };
             }
         }
-    };
+    }[modules[0] + 'Dispatch']);
 };
