060b598e96896f11f2afb0a5ed0e8da7f500236a
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-07-14 13:12:17 +0300

    feat: handle outcome [UTCORE-121]

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ module.exports = function(namespaces, imports = [], validations = [], destinatio
                 };
             }
 
+            outcome(params, $meta) {
+                [].concat(params).forEach(async({method, ...params}) => {
+                    if (method) {
+                        try {
+                            await this.bus.importMethod(method)(params, $meta);
+                        } catch (error) {
+                            this.error(error, $meta);
+                        }
+                    }
+                });
+            }
+
             handlers() {
                 return {
                     ...namespaces.reduce((prev, namespace) => ({
@@ -27,6 +39,13 @@ module.exports = function(namespaces, imports = [], validations = [], destinatio
                         } else {
                             return Promise.reject(this.bus.errors.methodNotFound({params: {method: $meta && $meta.method}}));
                         }
+                    },
+                    receive(msg, $meta) {
+                        if (msg && msg.$outcome) {
+                            this.outcome(msg.$outcome, $meta);
+                            delete msg.$outcome;
+                        }
+                        return msg;
                     }
                 };
             }
